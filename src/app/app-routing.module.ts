d41994b9122aee97a17a021a99088d373822c1dd
maheshnalkol <maheshnalkol500@gmail.com>
2025-05-19 01:16:27 +0530

    1st commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './shared/componentss/auth/auth.component';
+import { HomeComponent } from './shared/componentss/home/home.component';
+import { ContactComponent } from './shared/componentss/contact/contact.component';
+import { AboutComponent } from './shared/componentss/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'signup',
+    component: AuthComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthComponent } from './shared/componentss/auth/auth.component';
+import { MaterialModule } from './shared/material/material.module';
+import { NavbarComponent } from './shared/componentss/navbar/navbar.component';
+import { ContactComponent } from './shared/componentss/contact/contact.component';
+import { AboutComponent } from './shared/componentss/about/about.component';
+import { HomeComponent } from './shared/componentss/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, AuthComponent, NavbarComponent, ContactComponent, AboutComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
